efe96fe6277c4ec1cf44f09e88f1adc93c1895ca
Ajith T <ajith254t@gmail.com>
2025-07-14 05:17:04 -0800

    stack.js

---
+++ stack.js
@@ -0,0 +1,46 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.peek()); 
+stack.pop();
+console.log(stack.peek()); 
+console.log(stack.size()); 
+stack.print(); 
